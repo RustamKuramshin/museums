8d247a94e8944bfdb5f99035b892e3df813d167c
Rustam Kuramshin <kuramshin.py@yandex.ru>
2024-05-29 02:38:10 +0300

    commit

--- src/main/java/org/vaadin/example/Application.java
+++ src/main/java/org/vaadin/example/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@PWA(name = "Project Base for Vaadin with Spring", shortName = "Project Base")
+@PWA(name = "List of museums", shortName = "Museums")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
